c6bc246919010852c9c37fcbbab20fc9fae1eb3d
Yurifrancoso <yurirfrancoso@gmail.com>
2025-04-16 18:24:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+document.getElementById("messageButton").onclick = function() {
+    document.getElementById("message").innerHTML = "Acredite em si mesmo e em todas as suas capacidades. Lembre-se de que cada desafio é uma oportunidade disfarçada, uma chance de crescer e aprender.";
+};
